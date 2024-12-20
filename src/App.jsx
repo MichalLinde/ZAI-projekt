7dc2733c12b88a4cca1152858021248edc72dfb0
MichalLinde <mline@flyps.io>
2024-12-20 19:37:42 +0100

    test-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter, Route, Routes, Navigate } from 'react-router-dom';
 
-import "./App.css";
+import './App.css';
 
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import HomePage from "./pages/HomePage";
-import DetailsPage from "./pages/DetailsPage";
-import { useGlobalContext } from "./hooks/useGlobalContext";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import DetailsPage from './pages/DetailsPage';
+import { useGlobalContext } from './hooks/useGlobalContext';
 
 function App() {
-	const { mode } = useGlobalContext();
+  const { mode } = useGlobalContext();
 
-	return (
-		<div className="App" id={mode}>
-			<BrowserRouter basename="/ZAI-projekt">
-				<Header />
-				<Routes>
-					<Route path="/" element={<HomePage />} />
-					<Route path="/:countryCode" element={<DetailsPage />} />
-					<Route path="*" element={<Navigate to={"/"} replace />} />
-				</Routes>
-				<Footer />
-			</BrowserRouter>
-		</div>
-	);
+  return (
+    <div className="App" id={mode}>
+      <HashRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/:countryCode" element={<DetailsPage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+        <Footer />
+      </HashRouter>
+    </div>
+  );
 }
 
 export default App;
